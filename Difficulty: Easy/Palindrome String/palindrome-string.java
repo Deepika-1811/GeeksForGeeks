ea8b43fb6c29ccdafe1b1530365fb47e0db66b9f
Deepika-1811 <deepikadoddarapu@gmail.com>
2025-09-09 07:51:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome String/palindrome-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    boolean isPalindrome(String s) {
+        // code here
+        if(s == null || s.length()==0){
+            return true;
+        }
+        s = s.toLowerCase();
+        for(int i=0;i<s.length()/2;i++){
+            char start = s.charAt(i);
+            char end =s.charAt(s.length()-1-i);
+            if(start!=end){
+                return false;
+            }
+        }
+        return true;
+    }
+}
