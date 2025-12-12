ade9c04377d9bc288e72f35b337bbb177b66139b
Deepika-1811 <deepikadoddarapu@gmail.com>
2025-12-12 10:29:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Smallest Positive Missing/smallest-positive-missing.java
+++ Difficulty: Medium/Smallest Positive Missing/smallest-positive-missing.java
@@ -1,25 +1,18 @@
 class Solution {
     public int missingNumber(int[] arr) {
         // code here
-           int n = arr.length;
-
-        for (int i = 0; i < n; i++) {
-            // Place arr[i] to its correct position if possible
-            while (arr[i] > 0 && arr[i] <= n && arr[arr[i] - 1] != arr[i]) {
-                // Swap arr[i] with arr[arr[i] - 1]
-                int correctIndex = arr[i] - 1;
-                int temp = arr[i];
-                arr[i] = arr[correctIndex];
-                arr[correctIndex] = temp;
+        int n = arr.length;
+     boolean vis[] = new boolean[n];
+        for(int i=0;i<n;i++){
+            if(arr[i]>0 && arr[i]<=n){
+                vis[arr[i]-1] = true;
             }
         }
-
-        // Find the first place where index doesn't match value
-        for (int i = 0; i < n; i++) {
-            if (arr[i] != i + 1)
-                return i + 1;
+        for(int i=1;i<=n;i++){
+            if(!vis[i-1]){
+                return i;
+            }
         }
-
-        return n + 1;
+        return n+1;
     }
 }
